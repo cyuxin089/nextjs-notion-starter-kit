4b7d5b5734cbe4c195fa24a501e0388e401031e0
cyuxin089 <63711516+cyuxin089@users.noreply.github.com>
2021-11-21 21:00:50 +0800

    update

--- site.config.js
+++ site.config.js
@@ -17,9 +17,7 @@ module.exports = {
   socialImageSubtitle: 'Hi,i am Olivia~ 👋',
 
   // social usernames (optional)
-  // twitter: 'transitive_bs',
   // github: 'transitive-bullshit',
-  // linkedin: 'fisch2',
 
   // default notion icon and cover images for site-wide consistency (optional)
   // page-specific values will override these site-wide defaults
